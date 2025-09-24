3517f43741bd12117a3ca52ab63b3e480cc24f2f
Dhr-v20 <dhruvprajapati20503@gmail.com>
2025-09-24 14:05:55 +0530

    first commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState , useContext } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
-import { Context } from '../../context/context'
+import { Context } from '../../context/Context'
 const Sidebar = () => {
 
     const [extended, setExtended] = useState(false)
